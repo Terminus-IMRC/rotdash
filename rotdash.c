3a32e5316bfb0b892804a7f579fa3e21a6ab03f7
Terminus <i.can.speak.c.and.basic@gmail.com>
2013-12-07 21:10:06 +0900

    rotdash.c: refactored

--- rotdash.c
+++ rotdash.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
 #include<unistd.h>
-int main(int argc,char**argv){
-	//const char arr[]={'.','o','O','o'};
-	const char arr[]={'|','/','-','\\'};
-	if(argc!=2)
-		return 1;
-	chdir("/proc");
-	argc=0;
-	while(!access(argv[1],F_OK)){
-		printf("%c\b",arr[argc++]);
-		if(argc==sizeof(arr)){
-			printf(".%c\b",arr[argc-1]);
-			argc=0;
-		}
-		fflush(stdout);
-		usleep(100000);
+
+int main(int argc, char *argv[])
+{
+    //const char arr[]={'.','o','O','o'};
+    const char arr[] = { '|', '/', '-', '\\' };
+    if (argc != 2)
+	return 1;
+    chdir("/proc");
+    argc = 0;
+    while (!access(argv[1], F_OK)) {
+	printf("%c\b", arr[argc++]);
+	if (argc == sizeof(arr)) {
+	    printf(".%c\b", arr[argc - 1]);
+	    argc = 0;
 	}
-	puts(".");
-	return 0;
+	fflush(stdout);
+	usleep(100000);
+    }
+    puts(".");
+    return 0;
 }
